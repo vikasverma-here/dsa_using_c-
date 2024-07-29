5cc4143a0d8f24c39e753646f1bea2bcf06bef3f
vikas verma <vickypatel83036@gmail.com>
2024-07-29 13:02:11 +0530

    pattern session one completed

---
+++ c++/pattern.cpp
@@ -0,0 +1,76 @@
+#include <iostream>
+using namespace std;
+
+// int main (){
+   
+
+
+//     int row , col;
+   
+//     for (row=1 ; row<=5;row++){
+//         for (col=1 ; col<=5;col++){
+//             cout<<"*" <<" ";
+//         }
+//         cout<<endl;
+//     }
+
+  
+    
+// }    
+
+// int main (){
+//     int row , col;
+//     for (row=1;row<=4;row++){
+//         for(col=1; col<= 5;col++){
+//             cout<<"10" << " ";
+//         }
+//         cout<<endl;
+//     }
+    
+// }
+
+// int main (){
+//     int row , col;
+//     for (row=1;row<=4;row++){
+//         for(col=5; col>= 1;col--){
+//             cout<<col<< " ";
+//         } 
+//         cout<<endl;
+//     }
+    
+// }
+// int main (){
+//     int row , col;
+//     for (row=1;row<=4;row++){
+//         for(col=1; col<=5;col++){
+//             cout<<col*col<< " ";
+//         } 
+//         cout<<endl;
+//     }
+    
+// }
+
+// int main (){
+//     int row , col;
+//     for (row=1;row<=4;row++){
+       
+//         for(col=1; col<=5;col++){
+//              char name = 'a'+ col-1;
+//             cout<<name<< " ";
+//         } 
+//         cout<<endl;
+//     }
+    
+// }
+// int main (){
+//     int row , col,count=1;
+//     for (row=1;row<=4;row++){
+       
+//         for(col=1; col<=5;col++){
+//             cout<<count << " ";
+//             count++;
+//         } 
+//         cout<<endl;
+//     }
+    
+// }
